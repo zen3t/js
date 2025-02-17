69f86783052ab63ee540cb019b403e7ce111f286
zen3t <josealvess6154@gmail.com>
2025-02-17 20:48:30 -0300

    Novos estudos

---
+++ curso-de-js/Aula08/js/script.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector("#titulo");
+titulo.textContent = "Comida";
+
+let testando = document.querySelectorAll(".box");
+document.write(testando[0].textContent);
+document.write(testando[1].textContent);
+document.write(testando[2].textContent);

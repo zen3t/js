7bddb20d090416b79047aabd8341de767b813e67
zen3t <josealvess6154@gmail.com>
2025-03-28 17:47:45 -0300

    Cria o BoardGame

--- matching-game/src/main.js
+++ matching-game/src/main.js
@@ -1,6 +1,8 @@
 import "./styles/settings/colors.css";
+import "./styles/generic/reset.css";
 import "./styles/elements/base.css";
-import CardGame from "./components/CardGame";
+//import CardGame from "./components/CardGame";
+import BoardGame from "./objects/BoardGame";
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame();
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+const $htmlBoardGame = BoardGame(6);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
---
+++ matching-game/src/objects/BoardGame/index.js
@@ -0,0 +1,9 @@
+import CardGame from "../../components/CardGame";
+function BoardGame(amountCards) {
+  const $htmlCardGame = CardGame();
+  const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+
+  return $htmlBoardGame;
+}
+
+export default BoardGame;
